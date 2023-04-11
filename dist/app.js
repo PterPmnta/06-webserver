aa6d84ed2c5c0555a61084a2fb39e1b4b7a0cb42
Pedro Pimienta M <pedro.pimienta.morales@gmail.com>
2023-04-11 17:31:54 -0500

    files generated by build command

---
+++ dist/app.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import path from 'path';
+import hbs from 'hbs';
+import * as dotenv from 'dotenv';
+dotenv.config();
+const app = express();
+const __dirname = path.resolve(path.dirname(''));
+app.set('view engine', 'hbs');
+hbs.registerPartials(path.join(__dirname, 'views', 'partials'));
+app.use(express.static('public'));
+app.get('/', (req, res) => {
+    res.render('home', {
+        nombre: 'Pedro Pimienta',
+        titulo: 'Curso Node'
+    });
+});
+app.get('/generic', (req, res) => {
+    res.render('generic', {
+        nombre: 'Pedro Pimienta',
+        titulo: 'Curso Node'
+    });
+});
+app.get('/elements', (req, res) => {
+    res.render('elements', {
+        nombre: 'Pedro Pimienta',
+        titulo: 'Curso Node'
+    });
+});
+app.listen(process.env.PORT, () => {
+    console.log(`Example app listening on port ${process.env.PORT}`);
+});
+//# sourceMappingURL=app.js.map
